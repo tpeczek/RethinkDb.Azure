390222f340429f7407dbc4dc30a69d58fd04763a
Tomasz Pęczek <tpeczek@gmail.com>
2024-02-20 13:24:00 +0100

    Requesting desired number of workers

--- src/RethinkDb.Azure.WebJobs.Extensions/Trigger/RethinkDbTargetScaler.cs
+++ src/RethinkDb.Azure.WebJobs.Extensions/Trigger/RethinkDbTargetScaler.cs
@@ -1,16 +1,35 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Host.Scale;
 
 namespace RethinkDb.Azure.WebJobs.Extensions.Trigger
 {
     internal class RethinkDbTargetScaler : ITargetScaler
     {
-        public TargetScalerDescriptor TargetScalerDescriptor => throw new NotImplementedException();
+        #region Fields
+        private readonly string _functionId;
+        #endregion
 
+        #region Properties
+        public TargetScalerDescriptor TargetScalerDescriptor { get; }
+        #endregion
+
+        #region Constructor
+        public RethinkDbTargetScaler(string functionId)
+        {
+            _functionId = functionId;
+
+            TargetScalerDescriptor = new TargetScalerDescriptor(_functionId);
+        }
+        #endregion
+
+        #region Methods
         public Task<TargetScalerResult> GetScaleResultAsync(TargetScalerContext context)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(new TargetScalerResult
+            {
+                TargetWorkerCount = 1
+            });
         }
+        #endregion
     }
 }
