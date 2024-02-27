103ee66f5bb2ab6b84a9637877bb27f661dd9c0b
Tomasz Pęczek <tpeczek@gmail.com>
2024-02-27 16:42:12 +0100

    Adding isolated worker model output binding

---
+++ demos/Demo.Azure.Functions.Worker.RethinkDb/Model/ThreadStats.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo.Azure.Functions.Worker.RethinkDb.Model
+{
+    internal class ThreadStats
+    {
+        public int WorkerThreads { get; set; }
+
+        public int MinThreads { get; set; }
+
+        public int MaxThreads { get; set; }
+
+        public override string ToString()
+        {
+            return $"Available: {WorkerThreads}, Active: {MaxThreads - WorkerThreads}, Min: {MinThreads}, Max: {MaxThreads}";
+        }
+    }
+}
---
+++ demos/Demo.Azure.Functions.Worker.RethinkDb/RethinkDbMultipleDocuments.cs
@@ -0,0 +1,10 @@
+﻿using RethinkDb.Azure.Functions.Worker.Extensions;
+
+namespace Demo.Azure.Functions.Worker.RethinkDb
+{
+    public class RethinkDbMultipleDocuments
+    {
+        [RethinkDbOutput(databaseName: "Demo", tableName: "ThreadStats", CreateIfNotExists = true, HostnameSetting = "RethinkDbHostname", UserSetting = "RethinkDbUser", PasswordSetting = "RethinkDbPassword")]
+        public IEnumerable<object> RethinkDbDocuments { get; set; }
+    }
+}
